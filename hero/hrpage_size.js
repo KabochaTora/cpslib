a7efef2ec16ee89ec292c4cf7937d4c7a72c8c7f
KabochaTora <44458681+KabochaTora@users.noreply.github.com>
2018-12-29 01:32:48 +0800

    update

--- hero/hrpage_size.js
+++ hero/hrpage_size.js
@@ -1,27 +1,57 @@
-window.onresize = adjuest;
-adjuest();
-function adjuest(){
+window.onresize = adjustsize;
+adjustsize();
+function adjustsize(){
+	var i = 0;
 	var htmlwindow = document.getElementsByTagName("html")[0];
 	var hh = window.innerHeight;
-	var tabpageh = 0.02 * hh;
+	var tabpageh = 0.00 * hh;
 	var tab = document.getElementsByClassName("tab");
 	var tabc = document.getElementsByClassName("tabcontent");
 	document.getElementById("pagewithtab").style.marginTop = tabpageh + "px" ;
 	if ( hh > 800 ) {
 		tab[0].style.height= "52px";
-		var i = 0;
 		for( i = 0; i < tabc.length; i++ ) {
-			tabc[i].style.height= hh * 0.97 - 52 + "px";
+			tabc[i].style.height= hh * 1.00 - 52 + "px";
 		}
 	}
 	else {
-		alert("tast")
 		tab[0].style.height= "6.5%";
-		var i = 0;
 		for( i = 0; i < tabc.length; i++ ) {
 			tabc[i].style.height= "93.5%";
 		}
 	}
 	var label = document.getElementById("show");
 	label.innerHTML = "width = "+window.innerWidth+";height="+window.innerHeight;
+}
+
+
+function toPercent(point){
+    var str=Number(point*100).toFixed(1);
+    str+="%";
+    return str;
+}
+
+window.onload=iniprgr()
+
+function iniprgr(){
+	setTimeout("changeWidth(1.0,1.2,0.8)","10")
+}
+
+function changeWidth(x,y,z)
+{
+	var r = x / 1.5;
+	var s = y / 1.5;
+	var t = z / 1.5;
+	elementx=document.getElementById('atkBar')
+	{
+		elementx.style.width=toPercent(r);
+	}
+	elementy=document.getElementById('defBar')
+	{
+		elementy.style.width=toPercent(s);
+	}
+	elementz=document.getElementById('lifBar')
+	{
+		elementz.style.width=toPercent(t);
+	}
 }
---
+++ leftbutton.js
@@ -0,0 +1,17 @@
+function btnomov(i) {
+	var lfbt = document.getElementsByClassName("leftbtn");
+		lfbt[i].style.width = "95%";
+		lfbt[i].style.color = "#ffffff";
+		lfbt[i].style.backgroundColor = "#00aaa6";
+		lfbt[i].style.fontWeight = "600";
+		lfbt[i].style.textDecoration = "none";
+}
+
+function btnomot(i) {
+	var lfbt = document.getElementsByClassName("leftbtn");
+		lfbt[i].style.width = "95%";
+		lfbt[i].style.color = "#00aaa6";
+		lfbt[i].style.backgroundColor = "rgba(255,255,255,0)";
+		lfbt[i].style.fontWeight = "normal";
+		lfbt[i].style.textDecoration = "none";
+}
