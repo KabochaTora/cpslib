6fa4be3c314fc1d2f9f38f3d579f9119187dd5b8
KabochaTora <44458681+KabochaTora@users.noreply.github.com>
2019-02-19 17:04:25 +0800

    update

--- hero/translation.js
+++ hero/translation.js
@@ -4,22 +4,31 @@ function showjpver() {
 	var i=0;
 	var cnver = document.getElementsByClassName("cnver");
 	var jpver = document.getElementsByClassName("jpver");
+	var cnlrc = document.getElementsByClassName("lrccn");
 	for(i=0;i<cnver.length;i++) {
 		cnver[i].style.display="none";
 	}
 	for(i=0;i<jpver.length;i++) {
 		jpver[i].style.display="inline";
 	}
+	for(i=0;i<cnlrc.length;i++) {
+		cnlrc[i].style.display="none";
+	}
+		
 }
 
 function showcnver() {
 	var i=0;
 	var cnver = document.getElementsByClassName('cnver');
 	var jpver = document.getElementsByClassName('jpver');
+	var cnlrc = document.getElementsByClassName("lrccn");
 	for(i=0;i<jpver.length;i++) {
 		jpver[i].style.display="none";
 	}
 	for(i=0;i<jpver.length;i++) {
 		cnver[i].style.display="inline";
+	}
+	for(i=0;i<cnlrc.length;i++) {
+		cnlrc[i].style.display="table-row";
 	}
 }
